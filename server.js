6797cf585c0a6c1a2b0bf41acdc8517c56fa3bbf
aarvanitii <arvanitgrainca.ag@gmail.com>
2016-10-13 23:33:58 +0200

    Website is more responsive, Installed Handlebars plugin etc.

--- server.js
+++ server.js
@@ -1,28 +1,104 @@
 var express = require("express");
 var app = express();
+var exphbs  = require('express-handlebars');
 var router = express.Router();
 var path = __dirname + '/views/';
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function (req, res) {
+    var detajetePersonave = [
+      {
+        imgUrl: "/photo.jpg",
+        emri: "Endrit",
+        mbiemri: "Bytyqi",
+        facebookUrl: "https://www.facebook.com/endritbytyqi?fref=ts",
+        fcbPictureUrl: "fb.png",
+        personId: "endriti"
+      },
+      {
+        imgUrl: "/photo1.jpg",
+        emri: "Auroni",
+        mbiemri: "Bytyqi",
+        facebookUrl: "http://www.facebook.com/aauronii",
+        fcbPictureUrl: "fb.png",
+        personId: "auroni"
+      },
+      {
+        imgUrl: "/photo.jpg",
+        emri: "Arvanit",
+        mbiemri: "Grainca",
+        facebookUrl: "http://www.facebook.com/aarvanitii",
+        fcbPictureUrl: "fb.png",
+        personId: "arvaniti"
+      },
+      {
+        imgUrl: "/agoni.jpg",
+        emri: "Agon",
+        mbiemri: "Rushiti",
+        facebookUrl: "https://www.facebook.com/aagonnr?fref=tsl",
+        fcbPictureUrl: "fb.png",
+        personId: "agoni"
+      },
+      {
+        imgUrl: "/imm.jpg",
+        emri: "Im",
+        mbiemri: "Ibrahimi",
+        facebookUrl: "https://www.facebook.com/imm.qo?fref=ts",
+        fcbPictureUrl: "fb.png",
+        personId: "imi"
+      },
+      {
+        imgUrl: "/egzon.jpg",
+        emri: "Egzon",
+        mbiemri: "Dauti",
+        facebookUrl: "https://www.facebook.com/EgzonDautii?fref=ts",
+        fcbPictureUrl: "fb.png",
+        personId: "egzoni"
+      }
+    ]
+    res.render('about', {data: detajetePersonave});
+});
+app.get('/contact', function (req, res) {
+    res.render('contact');
+});
+app.get('/orari', function (req, res) {
+          var orariPastrimit = [
+          {
+            "java": 1,
+            "dita": "E merkure",
+            "personi": "Arvaniti"
+          },
+          {
+            "java": 2,
+            "dita": "E Marte",
+            "personi": "Auroni"
+          },
+          {
+            "java": 3,
+            "dita": "E Premte",
+            "personi": "Imi"
+          },
+          {
+            "java": 4,
+            "dita": "E Enjte",
+            "personi": "Agoni"
+          },
+          ]
+
+    res.render('orari', {data: orariPastrimit});
+});
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
 });
 
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
-});
-
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
-});
-
-router.get("/contact",function(req,res){
-  res.sendFile(path + "contact.html");
-});
-
-router.get("/orari",function(req,res){
-    res.sendFile(path + "orari.html");
-});
 app.use(express.static(__dirname + '/images'));
 
 app.use("/",router);
